407c748f953098d53c0ba57a58cd9a6204b2be19
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-01-06 09:44:21 +0100

    06012022

--- src/Million.java
+++ src/Million.java
@@ -2,15 +2,14 @@ public class Million {
 
 	public static void main(String[] args) {
 		
-		int wachstum = 18, zeit = 0, i = 0;
+		int wachstum = 18, i = 0;
 		double b_anzahl = 118;
 		
 		while (b_anzahl <= 1000000) {
 			b_anzahl = b_anzahl + b_anzahl * wachstum / 100;
 			i++;
-			zeit = i;
 		}
-		System.out.println("Eien Milliongrenze überschreitet die Kultur in " + zeit + " Tage.");
+		System.out.println("Eine Milliongrenze überschreitet die Kultur in " + i + " Tage.");
 
 	}
 
